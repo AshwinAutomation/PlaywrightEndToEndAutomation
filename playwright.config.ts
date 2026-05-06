dde9cc4590c193864a36632d0a0daf22a5f8728d
Automation Empire <ashwinautomationworld@gmail.com>
2026-05-06 16:09:29 +0530

    github actions demo tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,7 +58,8 @@ export const baseConfig = defineConfig({
     navigationTimeout: 30_000,
     ignoreHTTPSErrors: true,
     video: "on",
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    headless: true
   },
 
   /* Configure projects for major browsers */
